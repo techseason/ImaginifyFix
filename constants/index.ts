fd925b69e1902302cce02b4faf98c14d93ae6ff2
techseason <amanchaudharykumar@gmail.com>
2024-06-12 12:31:40 +0530

    credit changed

--- constants/index.ts
+++ constants/index.ts
@@ -47,10 +47,10 @@ export const plans = [
     name: "Free",
     icon: "/assets/icons/free-plan.svg",
     price: 0,
-    credits: 20,
+    credits: 10,
     inclusions: [
       {
-        label: "20 Free Credits",
+        label: "10 Free Credits",
         isIncluded: true,
       },
       {
